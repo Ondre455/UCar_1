6f989c82de3ca42914c3cf6a6dfeffcd5fb3118d
Ondre455 <andrei.shmikow@yandex.ru>
2020-11-22 15:30:38 +0400

    Созданы классы автомобиля и категории автомобиля

---
+++ domain/UCar/Car.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UCar
+{
+    public class Car
+    {
+        public CarID ID { get; }
+        public string Model { get; }
+        public string Description { get; }
+        public List<Cathegory> Cathegories;
+
+        public Car(string model, string description, CarID id)
+        {
+            Model = model;
+            Description = description;
+            ID = id;
+        }
+
+        public void AddCathegory(Cathegory cathegory)
+        {
+            Cathegories.Add(cathegory);
+            cathegory.cars.Add(this);
+        }
+
+        public override bool Equals(object obj)
+        {
+            Car OtherCar = (Car)obj;
+            return this.ID == OtherCar.ID;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public class CarID
+        {
+            public readonly int IDValue;
+            public override string ToString()
+            {
+                return IDValue.ToString();
+            }
+        }
+    }
+    
+}
---
+++ domain/UCar/Cathegory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UCar
+{
+    public class Cathegory
+    {
+        public CathegoryID ID { get; }
+        public string Name { get; }
+        public List<Car> cars;
+
+        public class CathegoryID
+        {
+            public readonly int IDValue;
+            public override string ToString()
+            {
+                return IDValue.ToString();
+            }
+        }
+    }
+}
