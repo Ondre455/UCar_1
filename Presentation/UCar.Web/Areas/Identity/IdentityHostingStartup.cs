4d03a7a73ea5752e847e5baf2c3f63da910bbab4
BigBoss <ilya-ponomarev-2001@mail.ru>
2020-11-24 18:30:37 +0400

    наконец все работает

--- Presentation/UCar.Web/Areas/Identity/IdentityHostingStartup.cs
+++ Presentation/UCar.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -20,7 +20,12 @@ namespace UCar.Web.Areas.Identity
                     options.UseSqlServer(
                         context.Configuration.GetConnectionString("AuthUsersDbContextConnection")));
 
-                services.AddDefaultIdentity<AuthUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                services.AddDefaultIdentity<AuthUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                })
                     .AddEntityFrameworkStores<AuthUsersDbContext>();
             });
         }
