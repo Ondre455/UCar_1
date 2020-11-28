2e3e4cab4d11366f7d69cd2a3c157a84e4935f2e
Ondre455 <andrei.shmikow@yandex.ru>
2020-11-28 16:53:39 +0400

    Добавлен тестовый проект, класс Price и всякая мелочь

---
+++ UCar.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace UCar.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ domain/UCar/CarService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UCar
+{
+    public class CarService
+    {
+        public Car[] GetAllByQuery()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
